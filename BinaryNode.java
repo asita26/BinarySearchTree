5c4fd5f3e224505dfe0a5ea368a24ecce29e3ead
asita26 <asita98890@gmail.com>
2023-12-08 22:22:33 +0530

    Adding size method and checking if given BST was generated

--- BinaryNode.java
+++ BinaryNode.java
@@ -2,7 +2,7 @@ public class BinaryNode<T extends Comparable<T>> {
     T key;
     BinaryNode<T> left;
     BinaryNode<T> right;
-
+   // Constructor to intialise the node
     public BinaryNode(T key) {
         this.key = key;
         this.left = null;
@@ -10,6 +10,7 @@ public class BinaryNode<T extends Comparable<T>> {
 
     }
 
+  //Method for adding node
     public void addNode(T newNode) {
         if (newNode.compareTo(this.key) < 0) {
             if (this.left == null) {
@@ -27,6 +28,13 @@ public class BinaryNode<T extends Comparable<T>> {
 
     }
 
+    //Method for calculating size
+    public static <T extends Comparable<T>> int size(BinaryNode<T> root){
+        if(root == null) return 0;
+        return 1 + size(root.left) + size(root.right);
+    }
+
+    //Method for adding a node using comparable
     public static <T extends Comparable<T>> void inOrderTraversal(BinaryNode<T> root) {
         if (root != null) {
             inOrderTraversal(root.left);    // Step 1
--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -4,6 +4,28 @@ public class BinarySearchTree {
       BinaryNode<Integer> rootNode = new BinaryNode<>(56);
       rootNode.addNode(30);
       rootNode.addNode(70);
+      rootNode.addNode(22);
+      rootNode.addNode(40);
+      rootNode.addNode(60);
+      rootNode.addNode(95);
+      rootNode.addNode(11);
+      rootNode.addNode(65);
+      rootNode.addNode(3);
+      rootNode.addNode(16);
+      rootNode.addNode(63);
+      rootNode.addNode(67);
+
+      //Displaying the BST
       BinaryNode.inOrderTraversal(rootNode);
+      System.out.println();
+
+      //Displaying size of BST
+      int expected_size=13;
+      int BST_size=BinaryNode.size(rootNode);
+      if(expected_size == BST_size) {
+         System.out.println("Expected size of BST was: 13");
+         System.out.println("Size of generated BST is: " + BST_size);
+         System.out.println("Hence all nodes have been added into the BST.");
+      }
    }
 }
